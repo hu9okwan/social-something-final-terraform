7a71c3da098194f3065729eefaf5e004ea6105b1
hu9okwan <hugowkwan@gmail.com>
2021-12-07 14:55:52 -0800

    Outputs domain at the end of terraform apply

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@
 # Data that you want to see printed to the terminal
 # Probably the dns name for the load balancer
 
+# output "dns_name" {
+#     value = module.my_lb.dns_name
+# }
+
+output "domain_name" {
+    value = aws_route53_record.final.name
+}
